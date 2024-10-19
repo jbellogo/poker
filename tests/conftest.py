253f7ecaf7926d8cf83e36df8425811e85ac3b8f
Juan Bello <jbnermal3@gmail.com>
2024-10-18 21:01:24 -0400

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,18 @@ def pot_fix(player_list_fix):
     pot = Pot(bb_amount = BIG_BLIND, players = player_list_fix)
     return pot
 
+
+# @pytest.fixture
+# def player_actions(actions : list[tuple[str,int]]):
+#     player_actions_lst = []
+#     '''
+#     Assume three players, Actions go in modular order ie P1, P2, P3, P1, P2, P3,...
+#     ex actions = [('call', 20), ('raise',40), ('call', 40), ('call', 20)]
+#     '''
+#     for i in range(0,4):
+#         pid = (i%3)+1
+#         action, amount = actions[i]
+#         player_actions_lst.append(PlayerBetResponse(pid=pid, player_funds=50, action = action, amount_bet=amount))
+
+#     return player_actions_lst
+
--- tests/test_betting.py
+++ tests/test_betting.py
@@ -1,10 +1,24 @@
 import pytest
 from models import Deck, Player, Board, BoardStage, PlayerBetResponse, Pot
 from unittest.mock import Mock
+# from typing import List, Tuple
 
 
 import pprint
 
+
+def get_player_actions(actions : list[tuple[str,int]]):
+    player_actions = []
+    '''
+    Assume three players, Actions go in modular order ie P1, P2, P3, P1, P2, P3,...
+    ex actions = [('call', 20), ('raise',40), ('call', 40), ('call', 20)]
+    '''
+    for i in range(0,4):
+        pid = (i%3)+1
+        action, amount = actions[i]
+        player_actions.append(PlayerBetResponse(pid=pid, player_funds=50, action = action, amount_bet=amount))
+
+    return player_actions
 
 
 def test_betting_round1(monkeypatch, pot_fix):
@@ -48,17 +62,16 @@ def test_betting_round1(monkeypatch, pot_fix):
     })
 
 
+## passing betting arguments to player_actions fixture
+# betting_actions = [('call', 20), ('raise',40), ('call', 40), ('call', 20)]
+# @pytest.mark.parametrize('player_actions', [betting_actions], indirect=True)
 def test_betting_round2(monkeypatch, pot_fix):
     '''
     '''
     round = BoardStage.PREFLOP
-    pot_state = pot_fix.get_pot_state()
-    P1resp1 = PlayerBetResponse(pid=1, player_funds=50, action = "call", amount_bet=20, pot_state=pot_state)
-    P2resp = PlayerBetResponse(pid=2, player_funds=50, action = "raise", amount_bet=40, pot_state=pot_state)
-    P3resp = PlayerBetResponse(pid=3, player_funds=50, action = "call", amount_bet=40, pot_state=pot_state)
-    P1resp2 = PlayerBetResponse(pid=1, player_funds=50, action = "call", amount_bet=20, pot_state=pot_state)
+    actions = [('call', 20), ('raise',40), ('call', 40), ('call', 20)]
+    player_actions = get_player_actions(actions) # NEW
 
-    player_actions = [P1resp1, P2resp, P3resp, P1resp2]
     test_mock = Mock(side_effect=player_actions)
     monkeypatch.setattr(Player, "make_bet", test_mock)
     pot_fix.betting_round(round)
@@ -93,65 +106,62 @@ def test_betting_round2(monkeypatch, pot_fix):
     })
 
 
-def test_betting_round3(monkeypatch, pot_fix):
-    '''
-    '''
-    round = BoardStage.FLOP
-    # pot_state = pot_fix.get_pot_state()
-    
-    P1resp1 = PlayerBetResponse(pid=1, player_funds=50, action = "check", amount_bet=0)
-    P2resp1 = PlayerBetResponse(pid=2, player_funds=50, action = "raise", amount_bet=100)
-    P3resp = PlayerBetResponse(pid=3, player_funds=50, action = "raise", amount_bet=200)
-    P1resp2 = PlayerBetResponse(pid=1, player_funds=50, action = "call", amount_bet=200)
-    P2resp2 = PlayerBetResponse(pid=1, player_funds=50, action = "call", amount_bet=100)
-
-    player_actions = [P1resp1, P2resp1, P3resp, P1resp2, P2resp2]
-    test_mock = Mock(side_effect=player_actions)
-    monkeypatch.setattr(Player, "make_bet", test_mock)
-
-    ## Need to patch this to obtain correct calling prices for each player.
-    total_bets_made_pre_action = [0, 0, 0, 0, 100]
-    test_mock = Mock(side_effect=total_bets_made_pre_action)
-    monkeypatch.setattr(Player, "amount_bet_this_hand", test_mock)
 
 
-    pot_fix.betting_round(round)
-    hand_history = pot_fix.get_hand_history()['FLOP']
+# def test_betting_round3(monkeypatch, pot_fix):
+#     '''
+#     '''
+#     round = BoardStage.FLOP
+#     # pot_state = pot_fix.get_pot_state()
+#     actions = [('check', 0), ('raise',100), ('raise', 200), ('call', 200), ('call', 100)]
 
-    pprint.pp(hand_history)
-    assert(len(hand_history)==5) # 5 moves total
-    assert(hand_history[0]['player1']['pot_state'] == {
-      "call_amount" : 0,
-      "check_allowed" : True,
-      "minimum_raise" : 1,
-      "pot_size" : 0,
-    })
-    assert(hand_history[1]['player2']['pot_state'] == {
-      "call_amount" : 0,
-      "check_allowed" : True,
-      "minimum_raise" : 1,
-      "pot_size" : 0,
-    })
-    ## P2 raised 100
-    assert(hand_history[2]['player3']['pot_state'] == {
-      "call_amount" : 100,
-      "check_allowed" : False,
-      "minimum_raise" : 200,
-      "pot_size" : 100,
-    })
-    ## P3 raised 200
-    assert(hand_history[3]['player1']['pot_state'] == {
-      "call_amount" : 200,
-      "check_allowed" : False,
-      "minimum_raise" : 400,
-      "pot_size" : 300,
-    })
-    assert(hand_history[4]['player2']['pot_state'] == {
-      "call_amount" : 100,   ## P2 ONLY NEEDS TO 100 to call. @TODO important Call_amount changes per player!!!!
-      "check_allowed" : False,
-      "minimum_raise" : 400,
-      "pot_size" : 500,
-    })
+#     player_actions = get_player_actions(actions)
+#     test_mock = Mock(side_effect=player_actions)
+#     monkeypatch.setattr(Player, "make_bet", test_mock)
+
+#     ## Need to patch this to obtain correct calling prices for each player.
+#     total_bets_made_pre_action = [0, 0, 0, 0, 100]
+#     test_mock = Mock(side_effect=total_bets_made_pre_action)
+#     monkeypatch.setattr(Player, "amount_bet_this_hand", test_mock)
+
+
+#     pot_fix.betting_round(round)
+#     hand_history = pot_fix.get_hand_history()['FLOP']
+
+#     pprint.pp(hand_history)
+#     assert(len(hand_history)==5) # 5 moves total
+#     assert(hand_history[0]['player1']['pot_state'] == {
+#       "call_amount" : 0,
+#       "check_allowed" : True,
+#       "minimum_raise" : 1,
+#       "pot_size" : 0,
+#     })
+#     assert(hand_history[1]['player2']['pot_state'] == {
+#       "call_amount" : 0,
+#       "check_allowed" : True,
+#       "minimum_raise" : 1,
+#       "pot_size" : 0,
+#     })
+#     ## P2 raised 100
+#     assert(hand_history[2]['player3']['pot_state'] == {
+#       "call_amount" : 100,
+#       "check_allowed" : False,
+#       "minimum_raise" : 200,
+#       "pot_size" : 100,
+#     })
+#     ## P3 raised 200
+#     assert(hand_history[3]['player1']['pot_state'] == {
+#       "call_amount" : 200,
+#       "check_allowed" : False,
+#       "minimum_raise" : 400,
+#       "pot_size" : 300,
+#     })
+#     assert(hand_history[4]['player2']['pot_state'] == {
+#       "call_amount" : 100,   ## P2 ONLY NEEDS TO 100 to call. @TODO important Call_amount changes per player!!!!
+#       "check_allowed" : False,
+#       "minimum_raise" : 400,
+#       "pot_size" : 500,
+#     })
 
 
 
